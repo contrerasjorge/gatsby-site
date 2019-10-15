6077dbe598b4e178d5c7428e0208f93fa0f4569b
Jorge Contreras <jorge.contreras789@gmail.com>
2019-10-15 00:31:57 -0400

    Init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+import Head from "../components/head"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Head title="Home" />
+      <h1>Hello</h1>
+      <h2>I'm Jorge a JavaScript developer.</h2>
+      <p>
+        Need a developer? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
+  )
+}
+
+export default IndexPage
